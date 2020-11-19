44f516058581b26f52e52171f85ff9010ca23960
nacky235 <w@nacky.page>
2020-11-19 11:12:38 +0900

    add Cell

---
+++ NarouReaderV2/BookView/Cell/BookViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BookViewTableViewCell.swift
+//  NarouReaderV2
+//
+//  Created by 稲葉夏輝 on 2020/11/19.
+//
+
+import UIKit
+
+class BookViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
